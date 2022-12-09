449a59e398067c51c31b0c8e221d3e468cf731eb
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2022-12-09 15:58:43 +0100

    mongoose

--- src/app.js
+++ src/app.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const models = require('.user.model');
 const app = express();
 
+const url = "mongodb+srv://root:root@clusterynovjs2022.vyzrjhs.mongodb.net/?retryWrites=true&w=majority"
+
+async function main() {
+    await mongoose.connect(url);
+}
+
+const userM = mongoose.model('user', userSchema)
+
 app.use(bodyParser.json());
+
+
+
+
+
+app.post('/auth/register', function(req, res) {
+    const newUser = new User({
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+        email:req.body.email,
+        password:req.body.password
+    })
+    newUser.save().then((user) => {
+        res.send(user);
+    })
+    .catch(err => {
+        res.status(404).send(err);
+    })
+})
+
 
 // first route GET
 app.get('/', function(req, res) {
