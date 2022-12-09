605a39bddfd1ebf58576f975097a03948f478338
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2022-12-09 13:15:35 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+// first route GET
+app.get('/', function(req, res) {
+    // console.log('test');
+    res.send({
+        message: "200 OK",
+        auth: false,
+        myproducts: [ { id: 1 }, { id: 2 } ]
+    });
+});
+
+app.post('/api/v1/auth/login', function(req, res) {
+    // console.log(req.body);
+    if (!req.body.email) {
+        return res.status(404).send({
+            auth: false,
+            message: "User not found"
+        })
+        res.send({
+            auth: true,
+            message: "User logged"
+        })
+    }
+})
+
+
+const myUser = JSON.stringify( {
+    firstName: "Zebi",
+    lastName: "Dess"
+});
+
+console.log(JSON.parse(myUser));
+
+app.listen("4000", function() {
+    console.log('Server Launch');
+});
