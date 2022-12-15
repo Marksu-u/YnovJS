d2321c64a76c39abf9e99fa9e2526de6888f18a0
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2022-12-15 12:56:37 +0100

    src folder

---
+++ front/src/components/TitlePage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const index = () => {
+    return (
+            <h1>My page</h1>
+    );
+}
+
+export default index;
---
+++ front/src/pages/index.js
@@ -0,0 +1,10 @@
+import TitlePage from '../components/TitlePage'
+
+export default function Home() {
+  return (
+    <div>
+      <TitlePage title="Home" />
+      <p>Some content</p>
+    </div>
+  )
+}
