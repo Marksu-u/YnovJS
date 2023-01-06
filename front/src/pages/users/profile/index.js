52349673828b9fe0dce242f2c5d76831104f3024
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2023-01-06 15:43:47 +0100

    profile

---
+++ front/src/pages/users/profile/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { TitlePage } from "../../../components/TitlePage";
+
+
+const Index = () => {
+    return (
+
+        <div>
+            <TitlePage title="Profile" />
+        </div>
+        );
+    }
+
+export default Index;
