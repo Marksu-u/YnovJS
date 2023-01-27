d5e10d06dc153642aef85a16031ee24e4e3727fa
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2023-01-27 09:56:32 +0100

    Create errorHandler.js
    Co-Authored-By: Sayuri <123705558+Say-ri@users.noreply.github.com>

---
+++ back/src/middlewares/errorHandler.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+    console.log(err);
+
+    // renvoyer une réponse avec le statut approprié
+    // réponse object avec les propriétés
+
+    success: false,
+    status: (err)
+    message:
+    env:
+
+    }
